030754d77c09cacddd7bbb2a252f298a34b1d21d
Emirhan Kalaycı <laxer61@hotmail.com>
2022-07-06 15:38:02 +0300

    +

--- eShopSimulator/Assets/Scripts/CompCam.cs
+++ eShopSimulator/Assets/Scripts/CompCam.cs
@@ -3,27 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CompCam : MonoBehaviour
-{   
+{
+    public GameObject instruction1;
+    public GameObject instruction2;
     public GameObject pcCam;
-    public KeyCode switchKey;
-  
-    void Update()
+
+    private void OnTriggerStay(Collider other)
     {
-        
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "pcArea")
+        if (other.tag == "pcArea")
         {
-            Debug.Log("girdi");
-            pcCam.SetActive(true);
-            /*
-            if (Input.GetKeyDown(switchKey))
+            instruction1.SetActive(true);     
+            
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                
-                
+                //instruction1.SetActive(false);
+                transform.position = new Vector3(77.241f, 2.881f, 16.843f);               
+            }                             
+        }
+
+        if (other.tag == "pcArea2")
+        {
+            instruction2.SetActive(true);
+            pcCam.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                transform.position = new Vector3(75.301f, 2.881f, 16.932f);
+                pcCam.SetActive(false);
+                instruction2.SetActive(false);
             }
-            */
+                
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "pcArea")
+        {
+            instruction1.SetActive(false);
         }
     }
 }
